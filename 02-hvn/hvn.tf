2a2c3ad10e46a2228021b7b90e7f804133463648
Andrei Burd <aburd@hashicorp.com>
2021-11-04 14:58:10 +0200

    worked a month ago

--- 02-hvn/hvn.tf
+++ 02-hvn/hvn.tf
@@ -16,11 +16,11 @@ data "terraform_remote_state" "vpc" {
 
 
 resource "hcp_aws_network_peering" "peer" {
-  hvn_id              = hcp_hvn.demo_hcp_hvn.hvn_id
-  peer_vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
-  peer_account_id     = data.terraform_remote_state.vpc.outputs.vpc_owner_id
-  peer_vpc_region     = var.region
-  peer_vpc_cidr_block = data.terraform_remote_state.vpc.outputs.cidr_block
+  hvn_id          = hcp_hvn.demo_hcp_hvn.hvn_id
+  peering_id      = "devopsdaystlv2021"
+  peer_vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+  peer_account_id = data.terraform_remote_state.vpc.outputs.vpc_owner_id
+  peer_vpc_region = var.region
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
@@ -34,3 +34,5 @@ resource "aws_route" "hvn-peering" {
   destination_cidr_block    = hcp_hvn.demo_hcp_hvn.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer.id
 }
+
+
