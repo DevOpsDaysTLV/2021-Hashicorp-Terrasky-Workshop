733d24d19bc89157796ceb688374ec0f93f7d3ec
Andrei Burd <aburd@hashicorp.com>
2021-11-04 14:27:28 +0200

    default values

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -1,5 +1,15 @@
-variable "name" {}
+variable "name" {
+  type    = string
+  default = "devopsdaystlv2021"
+}
 variable "public_subnets" {
   type = list(any)
+  default = [
+    "10.0.20.0/16",
+    "10.0.21.0/16",
+    "10.0.22.0/16",
+  ]
 }
-variable "cidr" {}
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
