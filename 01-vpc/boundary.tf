f51b854e5fd67130268dc81b1c6a69bd6e6b5c1a
Andrei Burd <aburd@hashicorp.com>
2021-11-16 13:09:51 +0200

    boundary cesurity group

--- 01-vpc/boundary.tf
+++ 01-vpc/boundary.tf
@@ -8,14 +8,34 @@ resource "aws_key_pair" "generated_key" {
   public_key = tls_private_key.dodworkshop.public_key_openssh
 }
 
+resource "aws_security_group_rule" "allow_inbound_ssh" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-resource "aws_instance" "web" {
+  security_group_id = module.vpc.default_security_group_id
+}
+
+resource "aws_security_group_rule" "allow_inbound_boundary" {
+  type        = "ingress"
+  from_port   = 9200
+  to_port     = 9202
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = module.vpc.default_security_group_id
+}
+
+
+resource "aws_instance" "boundary" {
   ami                         = "ami-0f4224d9a9b088c71"
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = module.vpc.public_subnets
   key_name                    = aws_key_pair.generated_key.key_name
-
+  security_groups             = [module.vpc.default_security_group_id]
 
   tags = {
     Name = "Boundary for devopsdays"
