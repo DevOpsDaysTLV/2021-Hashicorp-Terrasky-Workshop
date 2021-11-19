45f8904d532628728da64b879c4438a0c066edf6
Lev <lev.andelman@gmail.com>
2021-11-19 10:11:29 +0200

    working on cts

--- cts-demo.sh
+++ cts-demo.sh
@@ -21,7 +21,8 @@ sed "s#\"\"#\"$TF_VAR_tfc_organization_name\"#g" remote.tf.bck > remote.tf
 pe "cat remote.tf"
 pe "terraform init"
 pe 'export BOUNDARY_IP=$(terraform output --raw boundary_public_ip)'
-[ ! -f ../04-boundary-module/main.tf ] && cp ../04-boundary-module/main.tf ../04-boundary-module/main.tf.bck
+p "browse to http://${BOUNDARY_IP}:9200"
+[ ! -f ../04-boundary-module/main.tf.bck  ] && cp ../04-boundary-module/main.tf ../04-boundary-module/main.tf.bck
 sed "s#http://:9200#http://${BOUNDARY_IP}:9200#g"  ../04-boundary-module/main.tf.bck > ../04-boundary-module/main.tf
 
 cat > cts.hcl << EOF
@@ -94,6 +95,7 @@ spec:
 EOF
 pe 'cat cts.yaml'
 pe 'kubectl apply -f cts.yaml'
-pe 'kubectl get pods'
+pe 'kubectl wait pod/cts --for condition=ready'
 pe 'kubectl logs cts'
-pe 'kubectl exec -it cts -- cat sync-tasks/example-task/services_greetings.txt'
+pe 'kubectl exec -it cts -- cat /consul-terraform-sync/sync-tasks/example-task/terraform.tfvars'
+pe "browse to http://${BOUNDARY_IP}:9200"
