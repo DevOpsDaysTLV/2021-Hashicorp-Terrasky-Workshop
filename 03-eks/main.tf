99f235415b7810c742205e539e5236e548e769a3
Andrei Burd <aburd@hashicorp.com>
2021-11-21 17:31:37 +0200

    eks worker add default security group

--- 03-eks/main.tf
+++ 03-eks/main.tf
@@ -22,13 +22,22 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
+data "aws_security_group" "default" {
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_version = "1.21"
-  cluster_name    = "devopsdaytlv2021"
-  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnets         = data.terraform_remote_state.vpc.outputs.public_subnets
+  cluster_version                      = "1.21"
+  cluster_name                         = "devopsdaytlv2021"
+  vpc_id                               = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnets                              = data.terraform_remote_state.vpc.outputs.public_subnets
+  worker_additional_security_group_ids = [data.aws_security_group.default.id]
   worker_groups = [
     {
       instance_type = "m4.large"
